d0dd8d6d6a7e26901278dc0168cdce45513bf43b
Cahit Acar <cihat.acar4182@gmail.com>
2025-04-23 22:55:40 +0200

    Change package structure

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .load_datasets import txt_to_df, tdm_to_df, load_dataset
-
-__all__ = ["txt_to_df", "tdm_to_df", "load_dataset"]
---
+++ src/indenter/__init__.py
@@ -0,0 +1,25 @@
+# src/indenter/__init__.py
+
+"""
+Indenter
+========
+
+Tools for:
+ - loading indentation datasets (`load_datasets`)
+ - locating pop-ins (`locate`)
+ - exploring statistical models (`statistics`)
+ - constructing enriched datasets (`make_dataset`)
+"""
+
+__version__ = "0.1.0"
+
+# expose sub-modules at the package level
+from . import load_datasets, locate, statistics, make_dataset
+
+# define what "from indenter import *" pulls in
+__all__ = [
+    "load_datasets",
+    "locate",
+    "statistics",
+    "make_dataset",
+]
