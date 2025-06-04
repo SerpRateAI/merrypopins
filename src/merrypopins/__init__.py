441d7dd3a34870823eea0db5f32378bce25ada36
Cahit Acar <cihat.acar4182@gmail.com>
2025-06-04 10:36:02 +0200

    Trigger release pipeline

--- src/merrypopins/__init__.py
+++ src/merrypopins/__init__.py
@@ -10,7 +10,7 @@ Tools for:
  - constructing enriched datasets (`make_dataset`)
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 
 # expose sub-modules at the package level
 from . import load_datasets, preprocess, locate, statistics, make_dataset
